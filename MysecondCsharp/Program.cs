b6fbb405d54fb8bb2cd401c4f0b26c3f75e67c61
Āśutōṣa <www.aashutoshsinha888@gmail.com>
2022-01-30 17:19:30 +0545

    Add project files.

---
+++ MysecondCsharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MysecondCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello Aashutosh");
+            Console.WriteLine("I love C# Sharp");
+            Console.WriteLine("I am a Computer Science");
+            Console.Write("Ich bin Aashutosh");
+            Console.ReadLine();
+        }
+    }
+}
